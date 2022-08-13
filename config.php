61e2cc7e65463919b9732103d888e3b378690565
Silvio Diniz de Medeiros <silviodiniz@uol.com.br>
2022-08-13 20:57:42 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
   <head>
@@ -20,27 +19,4 @@
     ?>
 
   </body>
-=======
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-  </head>
-  <body>
-
-    <?php
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $banco = "empresa";
-        $conexao = new mysqli($host,$user,$pass,$banco);
-
-            if (mysqli_connect_errno()) {
-                die('Não foi possível conectar-se ao banco de dados: ' . mysqli_connect_error());
-            exit();
-        }
-    ?>
-
-  </body>
->>>>>>> 59f7530a47230756c9e584f1369ab35b68e9ab1e
 </html>  
